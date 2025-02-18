3eb4c66baf2c205ff7ec71a3c9246e33160fe0e4
Othmanali02 <othman90hijawi@gmail.com>
2025-02-18 17:11:06 -0500

    gridscore and fieldbook export functions

--- routes/listRoutes.js
+++ routes/listRoutes.js
@@ -1,10 +1,233 @@
 const express = require("express");
 const expressAsyncHandler = require("express-async-handler");
 require("dotenv").config();
+const { createObjectCsvWriter } = require("csv-writer");
 const axios = require("axios");
 const generateID = require("../services/tools.js");
+const path = require("path");
+const fs = require("fs");
 
 const listRouter = express.Router();
+
+function mapFormat(dataType) {
+	const typeMap = {
+		Code: "text",
+		Nominal: "categorical",
+		Date: "date",
+		Numerical: "numeric",
+		Ordinal: "categorical",
+		Duration: "numeric",
+		Text: "text",
+	};
+
+	return typeMap[dataType] ?? "text";
+}
+
+function mapGridscore(dataType) {
+	const typeMap = {
+		Code: "text",
+		Nominal: "categorical",
+		Date: "date",
+		Numerical: "float",
+		Ordinal: "categorical",
+		Duration: "range",
+		Text: "text",
+	};
+
+	return typeMap[dataType] ?? "text";
+}
+
+function mapJsonData(item) {
+	const categories = item.scale?.validValues?.categories;
+	const min = item.scale?.validValues?.minimumValue ?? 0;
+	const max = item.scale?.validValues?.maximumValue ?? 99999;
+
+	let restrictions = {};
+	if (categories && categories.length > 0) {
+		restrictions.categories = categories; 
+	} else if (min !== 0 || max !== 99999) {
+		restrictions.min = min;
+		restrictions.max = max;
+	}
+
+	return {
+		brapiId: item.observationVariableDbId ?? "",
+		name: item.observationVariableName ?? "",
+		description: item.trait?.description ?? null,
+		dataType: mapGridscore(item.scale?.dataType) ?? "text",
+		allowRepeats: true,
+		setSize: item.setSize ?? 1,
+		restrictions:
+			Object.keys(restrictions).length > 0 ? restrictions : undefined,
+		timeframe: item.timeframe ?? null,
+	};
+}
+
+function mapTsvData(item) {
+	return {
+		Name: item.observationVariableName ?? "",
+		"Short Name": item.trait?.traitName ?? "",
+		Description: item.trait?.description ?? "",
+		"Data Type": mapGridscore(item.scale?.dataType) ?? "text",
+		"Unit Name": item.scale?.scaleName ?? "",
+		"Unit Abbreviation": item.scale?.units ?? "",
+		"Unit Descriptions": item.scale?.description ?? "",
+		"Trait categories": item.scale?.validValues?.categories?.join(", ") ?? "",
+		Min: item.scale?.validValues?.minimumValue ?? "",
+		Max: item.scale?.validValues?.maximumValue ?? "",
+	};
+}
+
+function createTsvFile(listData, listName) {
+	const tsvData = listData.map(mapTsvData);
+
+	const headers = [
+		"Name",
+		"Short Name",
+		"Description",
+		"Data Type",
+		"Unit Name",
+		"Unit Abbreviation",
+		"Unit Descriptions",
+		"Trait categories",
+		"Min",
+		"Max",
+	];
+
+	const tsvContent = [
+		headers.join("\t"), // Join headers with tab
+		...tsvData.map((item) => Object.values(item).join("\t")), // Join each row's values with tab
+	].join("\n");
+
+	const filePath = path.join(__dirname, `${listName}-FieldBook.tsv`);
+
+	return tsvContent;
+}
+
+function createJsonFile(listData, listName) {
+	const jsonData = listData.map(mapJsonData);
+
+	const filePath = path.join(__dirname, `${listName}-FieldBook.json`);
+
+	return JSON.stringify(jsonData, null, 2); // Return JSON stringified with indentation
+}
+
+listRouter.post(
+	"/create-tsv-gridscore",
+	expressAsyncHandler(async (req, res) => {
+		try {
+			let listName = req.body.listName;
+			let listData = req.body.listData;
+
+			const tsvContent = createTsvFile(listData, listName);
+
+			res.setHeader("Content-Type", "text/tab-separated-values");
+			res.setHeader(
+				"Content-Disposition",
+				`attachment; filename=${listName}-Gridscore-tsv.tsv`
+			);
+
+			res.send(tsvContent);
+		} catch (error) {
+			console.error("Error making the POST request:", error);
+			res
+				.status(500)
+				.json({ message: "Internal Server Error", error: error.message });
+		}
+	})
+);
+
+listRouter.post(
+	"/create-csv",
+	expressAsyncHandler(async (req, res) => {
+		try {
+			let listName = req.body.listName;
+			console.log(req.body.listData);
+
+			const mappedData = req.body.listData.map((item, index) => ({
+				trait: item.observationVariableName ?? null,
+				format: mapFormat(item.scale?.dataType ?? "text"),
+				defaultValue: item.defaultValue ?? null,
+				minimum: item.scale?.validValues?.minimumValue ?? null,
+				maximum: item.scale?.validValues?.maximumValue ?? null,
+				details: item.trait?.description ?? null,
+				categories: item.scale?.validValues?.categories ?? null,
+				isVisible: true,
+				realPosition: index + 1,
+			}));
+
+			const headers = [
+				{ id: "trait", title: "trait" },
+				{ id: "format", title: "format" },
+				{ id: "defaultValue", title: "defaultValue" },
+				{ id: "minimum", title: "minimum" },
+				{ id: "maximum", title: "maximum" },
+				{ id: "details", title: "details" },
+				{ id: "categories", title: "categories" },
+				{ id: "isVisible", title: "isVisible" },
+				{ id: "realPosition", title: "realPosition" },
+			];
+
+			const csvWriter = createObjectCsvWriter({
+				path: path.join(__dirname, `${listName}-FieldBook.csv`),
+				header: headers,
+			});
+
+			csvWriter
+				.writeRecords(mappedData)
+				.then(() => {
+					console.log("CSV file created successfully!");
+
+					res.download(
+						path.join(__dirname, `${listName}-FieldBook.csv`),
+						"FieldBook.csv",
+						(err) => {
+							if (err) {
+								console.error("Error sending the file:", err);
+								res.status(500).send({ error: "Failed to send the CSV file" });
+							} else {
+								console.log("CSV file sent successfully");
+							}
+						}
+					);
+				})
+				.catch((err) => {
+					console.error("Error creating CSV:", err);
+					res.status(500).send({ error: "Failed to create CSV file" });
+				});
+		} catch (error) {
+			console.error("Error making the POST request:", error);
+			res
+				.status(500)
+				.json({ message: "Internal Server Error", error: error.message });
+		}
+	})
+);
+
+listRouter.post(
+	"/create-json-gridscore",
+	expressAsyncHandler(async (req, res) => {
+		try {
+			let listName = req.body.listName;
+			let listData = req.body.listData;
+
+			const jsonContent = createJsonFile(listData, listName);
+
+			res.setHeader("Content-Type", "application/json");
+			res.setHeader(
+				"Content-Disposition",
+				`attachment; filename=${listName}-Gridscore-JSON.json`
+			);
+
+			res.send(jsonContent);
+		} catch (error) {
+			console.error("Error making the POST request:", error);
+			res
+				.status(500)
+				.json({ message: "Internal Server Error", error: error.message });
+		}
+	})
+);
 
 listRouter.post(
 	"/getUserLists",
